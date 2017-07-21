de4f82949b261702f49c2b1abefaf62418c25ffe
Chris Cox <chriscox@abandonedfactory.net>
2017-07-21 12:59:40 -0700

    Simplifying routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,7 @@ Rails.application.routes.draw do
   resources :users
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
-  put '/users/:id', to: 'users#update'
-  patch '/users/:id', to: 'users#update'
-
+  
   get '/login',    to: 'sessions#new'
   post '/login',   to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
