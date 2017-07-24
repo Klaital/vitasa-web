169b37bce33c2ad76e247435432e555cf93ccf97
Chris Cox <chriscox@abandonedfactory.net>
2017-07-24 07:20:55 -0700

    The list of sites should be the default landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :sites
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'pages#about'
+  root 'sites#index'
 
   # Static Pages
   get '/about', to: 'pages#about'
