7d52ca04c1cab457aab3073b25791de86dc5c3af
Chris Cox <chriscox@abandonedfactory.net>
2017-07-25 12:23:49 -0700

    Display the SC as email

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -100,6 +100,7 @@ class SitesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_site
       @site = Site.find(params[:id])
+      @sitecoordinator = User.find(@site.sitecoordinator)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
