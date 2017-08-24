1ef9e425c434810dac20a33135652b53a3b69f0d
Chris Cox <chriscox@abandonedfactory.net>
2017-08-24 11:10:43 -0700

    Adding staging environment

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -1,5 +1,5 @@
 
 Rails.application.configure do 
     config.statsd_enabled = true
-    config.statsd = Statsd.new(Rails.env.production? ? '34.210.183.223' : 'localhost', 8125).tap{|sd| sd.namespace = "vitasa.#{Rails.env}"}
+    config.statsd = Statsd.new(Rails.env.production? || Rails.env.staging? ? '34.210.183.223' : 'localhost', 8125).tap{|sd| sd.namespace = "vitasa.#{Rails.env}"}
 end
