78f0e6fc9095866d2ddc21895056d967d535be92
Chris Cox <chriscox@abandonedfactory.net>
2017-09-11 14:10:42 -0700

    Enforce slug uniqueness

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,11 @@
 class Resource < ApplicationRecord
+    translates :text
+
+    validates :slug, {
+        uniqueness: true,
+        format: {
+            with: /\A[a-z0-9-]+-?(-[a-z0-9-]+)*\z/,
+            message: 'Must be a valid URL segment, using lowercase latin characters and single dashes. Leave blank to have a slug auto-generated from your sitename.'
+        }
+    }
 end
