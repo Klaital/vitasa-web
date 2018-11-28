ce213a553c03dacffada7efe3f9006b349fd5970
Chris Cox <chriscox@abandonedfactory.net>
2018-11-27 20:17:46 -0800

    Fixing date validator

--- app/models/work_log.rb
+++ app/models/work_log.rb
@@ -2,6 +2,6 @@ class WorkLog < ApplicationRecord
   belongs_to :user
   belongs_to :site
   validates :site_id, :presence => true
-  validates :date, :format => { :with => /\A\d4-\d2-\d2\z/, :message => 'Must be YYYY-MM-DD' }
+  validates :date, :format => { :with => /\A\d{4}-\d{2}-\d{2}\z/, :message => 'Must be YYYY-MM-DD' }
   validates :hours, :numericality => {:greater_than => 0}
 end
