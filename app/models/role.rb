06f0dd2b3342466843c9a6815e9fe4d4f9d318c3
Chris Cox <chriscox@abandonedfactory.net>
2018-11-27 19:49:42 -0800

    Adding Mobile role

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,7 +2,7 @@ class Role < ApplicationRecord
     has_many :users, through: :role_grants
     has_many :role_grants
 
-    VALID_ROLE_NAMES = %w{ Volunteer SiteCoordinator SiteCoordinatorInactive Admin }
+    VALID_ROLE_NAMES = %w{ Mobile Volunteer SiteCoordinator SiteCoordinatorInactive Admin }
     validates :name, inclusion: { 
             in: VALID_ROLE_NAMES,
             message: "%{value} is not a valid role name" 
