ca24f4e8d2f1d7830e7e23a6bada7f4783b71bf2
Chris Cox <chriscox@abandonedfactory.net>
2018-12-09 20:36:56 -0800

    Enable SES email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,9 +57,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "rails-base_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.perform_deliveries = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+      :address => 'email-smtp.us-west-2.amazonaws.com',
+      :port => 587,
+      :user_name => ENV["SES_SMTP_USERNAME"], #Your SMTP user
+      :password => ENV["SES_SMTP_PASSWORD"], #Your SMTP password
+      :authentication => :login,
+      :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
