7d4b9d6ee7db8593afc18ecc6c5cba8e5eca7773
Chris Cox <chriscox@abandonedfactory.net>
2018-12-11 18:24:12 -0800

    Send site notifications on calendar changes. Fixes #49

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,6 +1,15 @@
 class Calendar < ApplicationRecord
   belongs_to :site
 
+  after_update do
+    self.site.send_mobile_team_notification
+    self.site.send_preferred_site_notification
+  end
+  after_create do
+    self.site.send_mobile_team_notification
+    self.site.send_preferred_site_notification
+  end
+
   serialize :open, Tod::TimeOfDay
   serialize :close, Tod::TimeOfDay
 end
