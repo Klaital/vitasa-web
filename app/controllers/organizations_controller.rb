7ed10f95cbced11ca5613938dd5d186c315b62a1
Chris Cox <chriscox@abandonedfactory.net>
2019-09-22 13:45:49 -0700

    Removing erroneous caching statements from org

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,4 @@
 class OrganizationsController < ApplicationController
-  self.page_cache_directory = File.join(Rails.root, 'public', 'cached_pages')
-  caches_page :index
-
   # GET /organizations
   # GET /organizations.json
   def index
