0c1b6116651087971d15e1e11bdb1ec50d750134
Chris Cox <ccox@firststrikegames.com>
2019-09-25 10:39:37 -0700

    Adding Create Organization API

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,28 @@
 class OrganizationsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   # GET /organizations
   # GET /organizations.json
   def index
     @organizations = Organization.all
+  end
+
+  def create
+    unless logged_in?
+      logger.error("Must be logged in to create organization")
+      render json: {errors: "Must be logged in to create organization"}, status: :unauthorized
+      return
+    end
+    unless current_user.has_role?(['SuperAdmin'])
+      logger.error("Must be a SuperAdmin to create organization")
+      render json: {errors: "Must be a SuperAdmin to create organization"}, status: :unauthorized
+      return
+    end
+
+    org = Organization.new(name: params[:name])
+    if org.save
+      head :ok
+    else
+      render json: {errors: org.errors}, status: :bad_request
+    end
   end
 end
--- test/controllers/organizations_controller_test.rb
+++ test/controllers/organizations_controller_test.rb
@@ -9,4 +9,16 @@ class OrganizationsControllerTest < ActionDispatch::IntegrationTest
     org_data = JSON.parse(response.body)
     assert_equal(Organization.all.length, org_data.length, 'Incorrect org count in Index')
   end
+
+  test "superadmin can create an org" do
+    cookie = login_user('user-one', ['SuperAdmin'])
+    assert_difference('Organization.count', 1) do
+      post organizations_path, headers: {
+          'Content-Type' => 'application/json',
+      }, params: {
+          name: 'superadmin can create an org',
+      }.to_json
+      assert_response :success
+    end
+  end
 end
