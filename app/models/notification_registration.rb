568a729c691bc34f0430b8749fffc6aa58a25e2d
Chris Cox <chriscox@abandonedfactory.net>
2019-11-24 18:24:37 -0800

    Dynamically determine the platform enum

--- app/models/notification_registration.rb
+++ app/models/notification_registration.rb
@@ -22,17 +22,22 @@ class NotificationRegistration < ApplicationRecord
 
     # If the user already has a device registered, delete that Endpoint first
     NotificationRegistration.where(user_id: self.user_id).find_each do |registration|
+      logger.debug("Deleting old NR: #{registration}")
       next if registration.id == self.id
 
       # Deregister the endpoint and subscription from the SNS application
       unless registration.endpoint.nil?
+        logger.debug("Deleting old endpoint: #{registration.endpoint}")
         platform_endpoint = Aws::SNS::PlatformEndpoint.new(arn: registration.endpoint, :client => sns)      
         platform_endpoint.delete
       end
     
       unless registration.subscription.nil?
-        subscription = Aws::SNS::Subscription.new(arn: registration.subscription, :client => sns)
-        subscription.delete
+        if registration.subscription !~ /\A[0-9a-f]{8}-/
+          logger.debug("Deleting old subscription: #{registration.subscription}")
+          subscription = Aws::SNS::Subscription.new(arn: registration.subscription, :client => sns)
+          subscription.delete
+        end
       end
 
       # Then delete the NotificationRegistration
@@ -50,55 +55,57 @@ class NotificationRegistration < ApplicationRecord
     topics.flatten!
     
     topics.each do |t|
-        topic_arn = case t
-          when 'volunteers'
-            self.user.organization.volunteers_topic_arn
-          when 'sc'
-            self.user.organization.site_coordinators_topic_arn
-          else
-            t
-          end
-        sns_app_arn = case self.platform
-          when 'android'
-            Rails.configuration.sns_gcm_application_arn
-          when 'ios'
-            Rails.configuration.sns_apn_application_arn
-          when 'sms'
-            user.nil? ? '' : user.phone
-          else
-            ''
-          end
-
-        next if sns_app_arn.nil?
-
-
-        endpoint_arn = if self.platform == 'sms'
-          user.phone
+      protocol = 'application'
+      topic_arn = case t
+        when 'volunteers'
+          self.user.organization.volunteers_topic_arn
+        when 'sc'
+          self.user.organization.site_coordinators_topic_arn
         else
-          # Create a handle on the releant protocol's SNS Application
-          platform_application = Aws::SNS::PlatformApplication.new(arn: sns_app_arn, :client => sns)
-
-          # Construct a new platform endpoint for this user's device
-          platform_endpoint = platform_application.create_platform_endpoint({
-                                                                               token: self.token
-                                                                           })
-          # And save it for future housekeeping
-          self.endpoint = platform_endpoint.arn
-          platform_endpoint.arn
+          t
+        end
+      sns_app_arn = case self.platform
+        when 'android'
+          Rails.configuration.sns_gcm_application_arn
+        when 'ios'
+          Rails.configuration.sns_apn_application_arn
+        when 'sms'
+          protocol = 'sms'
+          user.nil? ? '' : user.phone
+        else
+          ''
         end
 
+      next if sns_app_arn.nil?
 
-        # Now we register that endpoint as a subscription on the relevant topics
-        subscription = sns.subscribe({
-            topic_arn: topic_arn,
-            protocol: 'application',
-            endpoint: endpoint_arn,
-        })
-        logger.info("SNS subscription: #{subscription}")
 
-        # And also save the subscription ARN for future housekeeping
-        self.subscription = subscription.subscription_arn
-        self.save
+      endpoint_arn = if self.platform == 'sms'
+        user.phone
+      else
+        # Create a handle on the releant protocol's SNS Application
+        platform_application = Aws::SNS::PlatformApplication.new(arn: sns_app_arn, :client => sns)
+
+        # Construct a new platform endpoint for this user's device
+        platform_endpoint = platform_application.create_platform_endpoint({
+                                                                             token: self.token
+                                                                         })
+        # And save it for future housekeeping
+        self.endpoint = platform_endpoint.arn
+        platform_endpoint.arn
+      end
+
+      # Now we register that endpoint as a subscription on the relevant topics
+      logger.debug("Subscribing to Topic #{topic_arn} with endpoint #{endpoint_arn}")
+      subscription = sns.subscribe({
+          topic_arn: topic_arn,
+          protocol: protocol,
+          endpoint: endpoint_arn,
+      })
+      logger.info("SNS subscription: #{subscription}")
+
+      # And also save the subscription ARN for future housekeeping
+      self.subscription = subscription.subscription_arn
+      self.save
     end
   end
 end
